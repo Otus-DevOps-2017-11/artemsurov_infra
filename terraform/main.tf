e8c1444cd091aa851cd0ad4cda4c5aebc961f7a3
Artem Surov <artemsurov@gmail.com>
2018-01-07 00:49:59 +0300

    Минимальная часть дз готова

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,48 @@
 provider "google" {
   version = "1.4.0"
   project = "${var.project}"
-  region = "${var.region}"
+  region  = "${var.region}"
 }
+
 resource "google_compute_instance" "app" {
-  name = "reddit-app"
+  name         = "reddit-app"
   machine_type = "g1-small"
-  zone = "europe-west1-b"
+  zone         = "${var.zone}"
+
   metadata {
-  sshKeys = "appuser:${file(var.public_key_path)}"
+    sshKeys = "appuser:${file(var.public_key_path)}"
   }
-# определение загрузочного диска
+
+  # определение загрузочного диска
   boot_disk {
     initialize_params {
-    image = "${var.disk_image}"
+      image = "${var.disk_image}"
     }
   }
-# определение сетевого интерфейса
+
+  # определение сетевого интерфейса
   network_interface {
     # сеть, к которой присоединить данный интерфейс
     network = "default"
+
     # использовать ephemeral IP для доступа из Интернет
     access_config {}
   }
+
   tags = ["reddit-app"]
+
   connection {
-    type = "ssh"
-    user = "appuser"
-    agent = false
-    private_key = "${file("~/.ssh/appuser")}"
-    }
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key)}"
+  }
+
   provisioner "file" {
-    source = "files/puma.service"
+    source      = "files/puma.service"
     destination = "/tmp/puma.service"
   }
+
   provisioner "remote-exec" {
     script = "files/deploy.sh"
   }
@@ -41,15 +50,19 @@ resource "google_compute_instance" "app" {
 
 resource "google_compute_firewall" "firewall_puma" {
   name = "allow-puma-default"
+
   # Название сети, в которой действует правило
   network = "default"
+
   # Какой доступ разрешить
   allow {
     protocol = "tcp"
-    ports = ["9292"]
+    ports    = ["9292"]
   }
+
   # Каким адресам разрешаем доступ
   source_ranges = ["0.0.0.0/0"]
+
   # Правило применимо для инстансов с тегом ...
   target_tags = ["reddit-app"]
 }
