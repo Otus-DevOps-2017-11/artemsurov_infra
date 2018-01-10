ffbf34779027c5c126790d95050a2b6144697215
Artem Surov <artemsurov@gmail.com>
2018-01-11 02:11:57 +0300

    Создал модули

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,16 @@
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable private_key {
+  description = "Private key for connection provisioner"
+}
+
+variable zone {
+  description = "Resource zone"
+  default     = "europe-west1-b"
+}
